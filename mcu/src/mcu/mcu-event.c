e9cc2c6c295fc59e164c1a3a414a16fdb71afc24
Tommy Alex <iptux7@gmail.com>
2015-07-19 17:55:51 +0800

    mcu: add event system
    Signed-off-by: Tommy Alex <iptux7@gmail.com>

---
+++ mcu/src/mcu/mcu-event.c
@@ -0,0 +1,130 @@
+/*
+ * mcu-event.c
+ * mcu bus, event define and handling
+ *
+ * Author: Alex.wang
+ * Create: 2015-07-18 20:38
+ */
+
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/sched.h>
+#include "mcu-event.h"
+
+
+static DEFINE_SPINLOCK(mcu_event_lock);	/* protects mcu_event_list */
+static LIST_HEAD(mcu_event_list);
+
+struct mcu_event *mcu_get_event(void)
+{
+	struct mcu_event *event = NULL;
+	unsigned long flags;
+
+	spin_lock_irqsave(&mcu_event_lock, flags);
+
+	if (!list_empty(&mcu_event_list)) {
+		event = list_first_entry(&mcu_event_list, struct mcu_event, node);
+		list_del_init(&event->node);
+	}
+
+	spin_unlock_irqrestore(&mcu_event_lock, flags);
+	return event;
+}
+
+void mcu_free_event(struct mcu_event *event)
+{
+	module_put(event->owner);
+	kfree(event);
+}
+
+void mcu_remove_duplicate_events(void *object, enum mcu_event_type type)
+{
+	struct mcu_event *e, *next;
+	unsigned long flags;
+
+	spin_lock_irqsave(&mcu_event_lock, flags);
+
+	list_for_each_entry_safe(e, next, &mcu_event_list, node) {
+		if (object == e->object) {
+			/*
+			 * If this event is of different type we should not
+			 * look further - we only suppress duplicate events
+			 * that were sent back-to-back.
+			 */
+			if (type != e->type)
+				break;
+
+			list_del_init(&e->node);
+			mcu_free_event(e);
+		}
+	}
+
+	spin_unlock_irqrestore(&mcu_event_lock, flags);
+}
+
+int __mcu_queue_event(void *object, struct module *owner, enum mcu_event_type event_type)
+{
+	unsigned long flags;
+	struct mcu_event *event;
+	int retval = 0;
+
+	spin_lock_irqsave(&mcu_event_lock, flags);
+
+	/*
+	 * Scan event list for the other events for the same serio port,
+	 * starting with the most recent one. If event is the same we
+	 * do not need add new one. If event is of different type we
+	 * need to add this event and should not look further because
+	 * we need to preseve sequence of distinct events.
+	 */
+	list_for_each_entry_reverse(event, &mcu_event_list, node) {
+		if (event->object == object) {
+			if (event->type == event_type)
+				goto out;
+			break;
+		}
+	}
+
+	event = kmalloc(sizeof(struct mcu_event), GFP_ATOMIC);
+	if (!event) {
+		pr_err("Not enough memory to queue event %d\n", event_type);
+		retval = -ENOMEM;
+		goto out;
+	}
+
+	if (!try_module_get(owner)) {
+		pr_warning("Can't get module reference, dropping event %d\n", event_type);
+		kfree(event);
+		retval = -EINVAL;
+		goto out;
+	}
+
+	event->type = event_type;
+	event->object = object;
+	event->owner = owner;
+
+	list_add_tail(&event->node, &mcu_event_list);
+	queue_work(system_long_wq, &mcu_event_work);
+
+out:
+	spin_unlock_irqrestore(&mcu_event_lock, flags);
+	return retval;
+}
+
+void mcu_remove_pending_events(void *object)
+{
+	struct mcu_event *event, *next;
+	unsigned long flags;
+
+	spin_lock_irqsave(&mcu_event_lock, flags);
+
+	list_for_each_entry_safe(event, next, &mcu_event_list, node) {
+		if (event->object == object) {
+			list_del_init(&event->node);
+			mcu_free_event(event);
+		}
+	}
+
+	spin_unlock_irqrestore(&mcu_event_lock, flags);
+}
+
---
+++ mcu/src/mcu/mcu-event.h
@@ -0,0 +1,53 @@
+/*
+ * mcu-event.h
+ * mcu bus, event define and handling
+ *
+ * Author: Alex.wang
+ * Create: 2015-07-19 12:11
+ */
+
+
+#ifndef __MCU_EVENT_H_
+#define __MCU_EVENT_H_
+
+#include <linux/module.h>
+#include "mcu-packet.h"
+
+
+#define MCU_EVENT_HANDLED	1
+#define MCU_EVENT_SENT	2
+#define MCU_EVENT_PENDING	3
+
+enum mcu_event_type {
+	MCU_DATA_RECEIVED,
+	MCU_WRITE_COMPLETE,
+	MCU_PING_DETECTED,
+	MCU_PONG_DETECTED,
+	MCU_CONTROL_REQUEST_DETECTED,
+	MCU_CONTROL_RESPONSE_DETECTED,
+	MCU_ATTACH_DRIVER,
+	MCU_SETUP_TTY,
+	MCU_CLOSE_TTY,
+};
+
+struct mcu_event {
+	enum mcu_event_type type;
+	unsigned long flags;
+	void *object;
+	struct module *owner;
+	struct list_head node;
+};
+
+
+extern struct work_struct mcu_event_work;
+
+struct mcu_event *mcu_get_event(void);
+void mcu_free_event(struct mcu_event *event);
+void mcu_remove_duplicate_events(void *object, enum mcu_event_type type);
+int __mcu_queue_event(void *object, struct module *owner, enum mcu_event_type event_type);
+void mcu_remove_pending_events(void *object);
+
+#define mcu_queue_event(object, type) __mcu_queue_event((object), THIS_MODULE, (type))
+
+#endif	// __MCU_EVENT_H_
+
