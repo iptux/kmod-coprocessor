5cc5dfa91f3022f7ab358cb7caa90ba6a8e8b64c
Tommy Alex <iptux7@gmail.com>
2015-07-20 03:00:52 +0800

    mcu: init mcu-bus.h
    Signed-off-by: Tommy Alex <iptux7@gmail.com>

---
+++ mcu/src/mcu/mcu-bus.h
@@ -0,0 +1,35 @@
+/*
+ * mcu-bus.h
+ * mcu coprocessor bus protocol internal header
+ *
+ * Author: Alex.wang
+ * Create: 2015-07-06 15:42
+ */
+
+
+#ifndef __MCU_BUS_H_
+#define __MCU_BUS_H_
+
+#include <linux/device.h>
+
+struct mcu_bus_device {
+	char name[MCU_NAME_SIZE];
+	struct device dev;
+
+	int (*late_init)(struct mcu_bus_device *);
+	int (*do_write)(struct mcu_bus_device *, const void *ptr, int len);
+	int nr;
+	void *pkt_data;
+	struct completion dev_released;
+	struct list_head children;
+};
+#define to_mcu_bus_device(d) container_of(d, struct mcu_bus_device, dev)
+
+extern void mcu_write_complete(struct mcu_bus_device *);
+extern int mcu_receive(struct mcu_bus_device *, const unsigned char *, size_t);
+
+extern int mcu_add_bus_device(struct mcu_bus_device *);
+extern void mcu_remove_bus_device(struct mcu_bus_device *);
+
+#endif	// __MCU_BUS_H_
+
