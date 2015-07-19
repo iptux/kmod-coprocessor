c276d483bd11f3173621a5f38567816e0abc5b99
Tommy Alex <iptux7@gmail.com>
2015-07-19 18:00:52 +0800

    mcu: event: add event wait support
    Signed-off-by: Tommy Alex <iptux7@gmail.com>

--- mcu/src/mcu/mcu-event.c
+++ mcu/src/mcu/mcu-event.c
@@ -11,6 +11,49 @@
 #include <linux/sched.h>
 #include "mcu-event.h"
 
+static DECLARE_WAIT_QUEUE_HEAD(mcu_wait_queue);
+static DEFINE_SPINLOCK(mcu_wait_lock);
+static LIST_HEAD(mcu_wait_list);
+
+void mcu_queue_wait(struct mcu_event *event)
+{
+	unsigned long flags;
+	spin_lock_irqsave(&mcu_wait_lock, flags);
+	list_add_tail(&event->node, &mcu_wait_list);
+	spin_unlock_irqrestore(&mcu_wait_lock, flags);
+}
+
+struct mcu_event *mcu_get_wait(enum mcu_event_type type)
+{
+	struct mcu_event *event, *next;
+	unsigned long flags;
+
+	spin_lock_irqsave(&mcu_wait_lock, flags);
+
+	list_for_each_entry_safe(event, next, &mcu_wait_list, node) {
+		if (type == event->type) {
+			list_del_init(&event->node);
+			spin_unlock_irqrestore(&mcu_wait_lock, flags);
+			return event;
+		}
+	}
+
+	spin_unlock_irqrestore(&mcu_wait_lock, flags);
+	return NULL;
+}
+
+int mcu_wait_event(struct mcu_event *event, int what, int timeout)
+{
+	mcu_queue_wait(event);
+	return wait_event_interruptible_timeout(mcu_wait_queue, test_bit(what, &event->flags), msecs_to_jiffies(timeout));
+}
+
+void mcu_wait_set(struct mcu_event *event, int what)
+{
+	set_bit(what, &event->flags);
+	wake_up(&mcu_wait_queue);
+}
+
 
 static DEFINE_SPINLOCK(mcu_event_lock);	/* protects mcu_event_list */
 static LIST_HEAD(mcu_event_list);
